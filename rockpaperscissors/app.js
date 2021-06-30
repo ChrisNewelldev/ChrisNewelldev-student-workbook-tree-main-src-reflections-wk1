33f3ac3573fd7759d0af97b5460277de9686cead
thedudeman69 <christopher.jp.newell@gmail.com>
2021-06-30 16:54:52 -0600

    all outputs printing to console

--- rockpaperscissors/app.js
+++ rockpaperscissors/app.js
@@ -1,15 +1,33 @@
-let playerChoice = {
-choice1:'rock',
-choice2:'paper',
-choice3:'scissors',
+let winConditions = []
+
+winConditions['paper'] = 'rock'
+winConditions['rock'] = 'scissors'
+winConditions['scissors'] = 'paper'
+
+function computerChoice(){
+  let choice = Math.floor(Math.random()*(3)+1)
+  switch(choice){
+    case 1:
+      return 'paper'
+    case 2:
+      return 'scissors'
+    case 3:
+      return 'rock'
+  }
 }
-
-
-
 function play(playerChoice){
-    
-    console.log(playerChoice)
+let cpChoice = computerChoice()
+if (playerChoice == winConditions[cpChoice]){
+  console.log('player wins')
 }
+else if (playerChoice === cpChoice){
+  console.log('draw')
+}
+else {console.log('computer wins')}
+}
+//play()
+
+
 
 
 
