3f1e8676b2262c9cb108f6e2dcfd2fc51b577fb3
thedudeman69 <christopher.jp.newell@gmail.com>
2021-07-02 11:50:32 -0600

    halfway there

--- checkpoint2/app.js
+++ checkpoint2/app.js
@@ -1,1 +1,56 @@
-function()
+let cash = 0;
+
+const clickUpgrades = {
+  shovel: {
+    price: 100,
+    quantity: 0,
+    multiplier: 1
+  },
+  bigshovel: {
+    price: 500,
+    quantity: 0,
+    multiplier: 5
+  }
+};
+
+const automaticUpgrades = {
+  employee: {
+    price: 600,
+    quantity: 0,
+    multiplier: 20
+  },
+  crew: {
+    price: 5000,
+    quantity: 0,
+    multiplier: 120
+  }
+}
+
+function buyshovel(
+  console.log("purchased")
+)
+
+function buybigshovel(
+  console.log("purchased")
+)
+
+function buyemployee(
+  console.log("purchased")
+)
+
+function buycrew(
+  console.log("purchased")
+)
+
+function mine (){
+(cash++)
+update()
+}
+
+
+function update() {
+
+  document.getElementById('cash').innerText = cash
+
+}
+
